d46fe799c1d370af13e9b4bd91e787b3d85c67bc
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-09 15:53:25 +0100

    aggiunta funzioni per i numeri casuali

--- js/script.js
+++ js/script.js
@@ -3,3 +3,35 @@
 // 3. mostra in pagina i numeri indovinati
     // 3.1 fare un ciclo che controlli i numeri inseriti dall'utente e che li metta in relazione con in numeri casuali
     // 3.2 mostrare solo quelli uguali
+
+
+// container
+const numCont = document.getElementById("container");
+
+// array per i numeri
+let arrNum = [];
+
+randomGenerator();
+
+console.log(arrNum);
+
+// funzioni
+function randomGenerator () {
+    while (arrNum.length < 5) {
+
+        let Num = Math.floor(Math.random() * 100) - 1;
+
+        let duplicati = false;
+
+        for (i = 0; i < arrNum.length; i++) {
+            if (Num === arrNum[i]) {
+                duplicati = true;
+            }
+        }
+
+        if (duplicati == false) {
+            arrNum.push(Num)
+        }
+     
+    }
+}
