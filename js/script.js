040ea9b76079a95f43884a8a23995fbc0c9818e0
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-09 15:46:12 +0100

    pseudocodice

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// 1. generare 5 numeri casuali senza ripetizioni e mostrarli per 30 secondi => setTimeout + Math
+// 2. aprire un prompt() per ogni numero in modo che l'utente possa inserire ogni numero => setTimeout
+// 3. mostra in pagina i numeri indovinati
+    // 3.1 fare un ciclo che controlli i numeri inseriti dall'utente e che li metta in relazione con in numeri casuali
+    // 3.2 mostrare solo quelli uguali
